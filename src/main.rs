7ce7c5462f932d0bbadad1c78700aa3056a79b7d
alex <pisonalex@gmail.com>
2026-02-24 21:48:49 +0100

    Feat: reading from the file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +7,10 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    dbg!(query);
-    dbg!(file_path);
+    dbg!(query, file_path);
+
+    let content = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    dbg!(content);
 }
