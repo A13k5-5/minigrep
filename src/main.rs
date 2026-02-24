1c1aa35b28a65851c9471775cb5f17f94eb8a8cf
alex <pisonalex@gmail.com>
2026-02-24 21:55:24 +0100

    Refactor: parse_config to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-
+    let (query, file_path) = parse_config(&args);
     dbg!(query, file_path);
 
     let content = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    dbg!(content);
+    println!("\n{}", content);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
